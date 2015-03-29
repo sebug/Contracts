1bf23b4686a286c78087604b320af938032fd5a8
sebug <sebastian.gfeller@gmail.com>
2015-03-29 16:28:20 +0200

    A bit more

--- FContracts.fs
+++ FContracts.fs
@@ -7,8 +7,10 @@ type Date = Date of DateTime
 type Currency = CHF | EUR
 
 type Contract =
-    | One of Date * Currency
+    | One of Currency
     | Zero
+    | Give of Contract
+    | And of Contract * Contract
 
 
 let date (ds: string): Date option =
@@ -21,4 +23,22 @@ let date (ds: string): Date option =
 let zero: Contract =
     Zero
 
+type Days = double
+
+let diff (d1: Date) (d2: Date): Days =
+    match (d1,d2) with
+        | (Date dt1,Date dt2) -> (dt2 - dt1).TotalDays
+
+let add (date: Date) (days: Days): Date =
+    match date with
+        | (Date date) -> Date (date.AddDays(days))
+
+let cand (c1: Contract) (c2: Contract): Contract =
+    And (c1,c2)
+
+let give (c: Contract): Contract =
+    Give c
+
+let andGive (c: Contract) (d: Contract): Contract =
+    cand c <| give d
 
